ef8a27d632bfe27b699d37011edf8db54dd8138c
pra-setiyo <prassonly@gmail.com>
2023-05-01 23:57:16 +0700

    edit text Markdow,input,previewer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const App = () => {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, "test"), /*#__PURE__*/React.createElement("p", null, "test"));
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Markdown Previewer"), /*#__PURE__*/React.createElement("p", null, "Input"), /*#__PURE__*/React.createElement("p", null, "Previewer"));
 };
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));

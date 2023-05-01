fc73ede8c35087ccf9a33e7fb3301fbfd5a8c1cb
pra-setiyo <prassonly@gmail.com>
2023-05-01 23:26:47 +0700

    add markdownPreviewer

---
+++ index.js
@@ -0,0 +1,4 @@
+const App = () => {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, "test"), /*#__PURE__*/React.createElement("p", null, "test"));
+};
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));

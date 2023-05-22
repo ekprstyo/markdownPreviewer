0ada5433e1db5e7c885901bc5e7f2ddfa5f91022
pra-setiyo <prassonly@gmail.com>
2023-05-22 14:49:11 +0700

    refresh

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const App = () => {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Markdown Previewer"), /*#__PURE__*/React.createElement("p", null, "Input"), /*#__PURE__*/React.createElement("p", null, "Previewer"));
+  const [input, setInput] = React.useState("#First line\n\n##Second Line\n\n###Third Line\n\nEtc...");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+    className: "editor shadow container"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "head"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "logo-name"
+  }, /*#__PURE__*/React.createElement("i", {
+    class: "fa-brands fa-free-code-camp"
+  }), /*#__PURE__*/React.createElement("h3", null, "Editor")), /*#__PURE__*/React.createElement("i", {
+    class: "fa-solid fa-maximize"
+  })), /*#__PURE__*/React.createElement("textarea", {
+    className: "editor-field field",
+    type: "text",
+    onChange: event => setInput(event.target.value)
+  }, input)), /*#__PURE__*/React.createElement("div", {
+    className: "previewer shadow container"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "head"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "logo-name"
+  }, /*#__PURE__*/React.createElement("i", {
+    class: "fa-brands fa-free-code-camp"
+  }), /*#__PURE__*/React.createElement("h3", null, "Previewer")), /*#__PURE__*/React.createElement("i", {
+    class: "fa-solid fa-maximize"
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "previewer-field field"
+  })));
 };
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));

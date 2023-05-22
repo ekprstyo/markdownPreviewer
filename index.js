1bd5dc8888db9da7ac93c9d80b12fb837a587c5f
pra-setiyo <prassonly@gmail.com>
2023-05-23 00:12:02 +0700

    add marked

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const App = () => {
-  const [input, setInput] = React.useState("#First line\n\n##Second Line\n\n###Third Line\n\nEtc...");
+  const [input, setInput] = React.useState("# Heading-1\n\n## Heading-2\n\n### Heading-3\n\n#### Heading-4\n\n##### Heading-5\n\n###### Heading-6");
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "editor shadow container"
   }, /*#__PURE__*/React.createElement("div", {
@@ -12,6 +12,7 @@ const App = () => {
     class: "fa-solid fa-maximize"
   })), /*#__PURE__*/React.createElement("textarea", {
     className: "editor-field field",
+    id: "editor",
     type: "text",
     onChange: event => setInput(event.target.value)
   }, input)), /*#__PURE__*/React.createElement("div", {
@@ -25,7 +26,11 @@ const App = () => {
   }), /*#__PURE__*/React.createElement("h3", null, "Previewer")), /*#__PURE__*/React.createElement("i", {
     class: "fa-solid fa-maximize"
   })), /*#__PURE__*/React.createElement("div", {
-    className: "previewer-field field"
+    className: "previewer-field field",
+    id: "preview",
+    dangerouslySetInnerHTML: {
+      __html: DOMPurify.sanitize(marked.parse(input))
+    }
   })));
 };
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
